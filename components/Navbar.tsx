290911d35c37161d1b26561ae89a7c0e444ff9fc
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-26 12:05:26 -0300

    navbar structure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <nav className="w-full flex bg-slate-500 h-20">
-      <h1>Shopping Cart</h1>
-      <div className="flex justify-evenly">
-        <a href="#">Shop</a>
-        <a href="#">Contact</a>
+    <nav className="w-full text-white flex bg-slate-500 justify-between items-center h-20 px-8">
+      <Link href="/" className="">
+        Shopping Cart
+      </Link>
+      <div className="flex justify-evenly items-center gap-20 h-full">
+        <Link href="/shop" className="">
+          Shop
+        </Link>
+        <Link href="/contact" className="">
+          Contact
+        </Link>
       </div>
     </nav>
   );
