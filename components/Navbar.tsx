628463d9d16ab489d98e1914ac969748ba948732
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-26 11:38:38 -0300

    tailwind

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full flex bg-slate-500 h-20">
+      <h1>Shopping Cart</h1>
+      <div className="flex justify-evenly">
+        <a href="#">Shop</a>
+        <a href="#">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
