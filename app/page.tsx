d90abf2ac743fc945d0cb16b4b77f36fd450146b
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-31 23:49:28 -0300

    Home UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import img1 from "../images/img1.webp";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="h-screen bg-black">
+        <Image src={img1} alt="MainImage" className="object-cover h-full" />
+      </div>
+    </main>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
-import { IoMenu } from "react-icons/io5";
+import {
+  IoMenu,
+  IoCartOutline,
+  IoSearchOutline,
+  IoCartSharp,
+} from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="w-full text-white flex bg-slate-500 justify-between items-center h-20 px-8">
-      <div className="flex w-full justify-evenly bg-red-200 items-center gap-8">
-        <IoMenu className="text-4xl" />
-        <Link href="/" className="">
-          Ropa
+    <nav className="w-full text-white flex fixed bg-black items-center h-20 px-4">
+      <div className="flex w-full items-center gap-8 text-5xl">
+        <IoMenu />
+        <Link href="/" className="w-full text-3xl">
+          Undefined
         </Link>
+        <IoSearchOutline />
+        <IoCartOutline />
       </div>
-      {/* <div className="flex justify-evenly items-center gap-20 h-full">
-        <Link href="/shop" className="">
-          Shop
-        </Link>
-        <Link href="/contact" className="">
-          Contact
-        </Link>
-      </div> */}
     </nav>
   );
 };
