bfd602249e0a32b5ee73ae91f5b5fccff7e02c9f
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-31 22:24:39 -0300

    Starting mobile-first

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <nav className="w-full text-white flex bg-slate-500 justify-between items-center h-20 px-8">
-      <Link href="/" className="">
-        Shopping Cart
-      </Link>
-      <div className="flex justify-evenly items-center gap-20 h-full">
+      <div className="flex w-full justify-evenly bg-red-200 items-center gap-8">
+        <IoMenu className="text-4xl" />
+        <Link href="/" className="">
+          Ropa
+        </Link>
+      </div>
+      {/* <div className="flex justify-evenly items-center gap-20 h-full">
         <Link href="/shop" className="">
           Shop
         </Link>
         <Link href="/contact" className="">
           Contact
         </Link>
-      </div>
+      </div> */}
     </nav>
   );
 };
