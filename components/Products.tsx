51625b14ea748a7d7a337c9e6398e2800d827319
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-06 22:16:29 -0300

    finished products(home) for mobile

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,6 +1,7 @@
 import hoodie from "@/images/HOODIEPrueba.webp";
 import hoodie1 from "@/images/HOODIEPrueba1.webp";
 import Product from "@/components/Product";
+import Link from "next/link";
 
 const Products = () => {
   return (
@@ -13,6 +14,13 @@ const Products = () => {
         <Product imgSrc={hoodie} />
         <Product imgSrc={hoodie} />
         <Product imgSrc={hoodie1} />
+      </div>
+      <div className="flex justify-center mt-4 text-sm w-full">
+        <div className="w-[70%] p-2 border-[1px] border-black">
+          <Link href="/shop" className="">
+            <h2 className="text-center">VER TODOS LOS PRODUCTOS</h2>
+          </Link>
+        </div>
       </div>
     </div>
   );
