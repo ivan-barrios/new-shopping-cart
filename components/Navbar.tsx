781c03c57f6969ecbdd37e212c6dacc90e220d30
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-09 19:57:14 -0300

    Navbar element position fixed (mobile)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,13 +9,18 @@ import {
 const Navbar = () => {
   return (
     <nav className="w-full text-black flex sticky bg-white items-center h-16 px-4">
-      <div className="flex w-full items-center gap-8 text-5xl">
-        <IoMenu />
-        <Link href="/" className="w-full text-3xl">
-          Undefined
-        </Link>
-        <IoSearchOutline />
-        <IoCartOutline />
+      <div className="flex w-full items-center text-3xl">
+        <div className="flex gap-4">
+          <IoMenu />
+          <Link href="/" className="text-3xl">
+            Undefined
+          </Link>
+        </div>
+        <div className="flex items-center gap-2 justify-end w-full md:gap-8">
+          <IoSearchOutline />
+          <IoCartOutline />
+          <span className="text-xs">(0)</span>
+        </div>
       </div>
     </nav>
   );
