ce2c3e10ad78dbeac7fee18e1a6c0b0fdb01974c
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-21 23:14:22 -0300

    Social in navMenu

--- components/subcomponents/NavMenu.tsx
+++ components/subcomponents/NavMenu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { TfiClose } from "react-icons/tfi";
+import { FaDiscord, FaInstagram } from "react-icons/fa";
 
 const NavMenu = ({ collapse, setCollapse }) => {
   return (
@@ -11,38 +12,42 @@ const NavMenu = ({ collapse, setCollapse }) => {
       <div className="flex justify-end p-4">
         <TfiClose
           onClick={() => setCollapse(false)}
-          className="cursor-pointer text-4xl"
+          className="cursor-pointer text-4xl hover:text-gray-600"
         />
       </div>
-      <div className="flex flex-col text-2xl items-center">
+      <div className="flex flex-col gap-4 text-2xl items-center">
         <a
-          className="p-4 bg-white w-full text-center hover:bg-slate-50"
+          className="p-4 bg-gray-100 w-full text-center hover:bg-slate-50"
           href="#home"
           onClick={() => setCollapse(false)}
         >
           Home
         </a>
         <a
-          className="p-4 bg-white w-full text-center hover:bg-slate-50"
+          className="p-4 bg-gray-100 w-full text-center hover:bg-slate-50"
           href="#search"
           onClick={() => setCollapse(false)}
         >
           Search
         </a>
         <a
-          className="p-4 bg-white w-full text-center hover:bg-slate-50"
+          className="p-4 bg-gray-100 w-full text-center hover:bg-slate-50"
           href="#cart"
           onClick={() => setCollapse(false)}
         >
           Cart
         </a>
         <a
-          className="p-4 bg-white w-full text-center hover:bg-slate-50"
+          className="p-4 bg-gray-100 w-full text-center hover:bg-slate-50"
           href="#contact"
           onClick={() => setCollapse(false)}
         >
           Contact
         </a>
+      </div>
+      <div className="relative top-[20%] flex justify-around w-full text-4xl">
+        <FaInstagram className="cursor-pointer" />
+        <FaDiscord className="cursor-pointer" />
       </div>
     </div>
   );
