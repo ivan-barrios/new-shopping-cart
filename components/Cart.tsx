9e65afc6ed8d25465e60ae25fc8a8e4bf4fccd7f
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-26 19:19:16 -0300

    Cart structure

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -13,20 +13,25 @@ const Cart = ({ collapseCart, setCollapseCart }) => {
         collapseCart ? "right-0" : "right-[-100vw]"
       } right-0 top-0 w-full bg-gray-100 h-screen transition-all ease-in-out duration-400 md:w-[400px] lg:w-[500px]`}
     >
-      <div className="flex justify-between w-full p-4">
+      <div className="flex justify-between items-center w-full h-[70px] p-4">
         <h1 className="text-xl">CARRITO DE COMPRAS</h1>
         <TfiClose
           onClick={() => setCollapseCart(false)}
           className="cursor-pointer text-2xl hover:text-gray-600"
         />
       </div>
-      <div className="h-[calc(100%-300px)]">
-        <CartProdcut />
+      <div className="h-[calc(100%-300px-70px)] px-8">
+        <div className="w-full h-full overflow-y-scroll">
+          <CartProdcut />
+          <CartProdcut />
+          <CartProdcut />
+          <CartProdcut />
+        </div>
       </div>
       <div className="absolute w-full h-[250px] flex flex-col justify-evenly items-center bottom-0 p-4 mb-8">
         <div className="flex justify-between items-center w-full p-4 border-b-[1px]">
           <h2>SUBTOTAL:</h2>
-          <h2>$15.900</h2>
+          <h2>$40.000</h2>
         </div>
         <div
           onClick={() => setArrowDown(!arrowDown)}
@@ -38,7 +43,7 @@ const Cart = ({ collapseCart, setCollapseCart }) => {
         </div>
         <div className="flex justify-between items-center w-full px-4 py-8 border-b-[1px] text-xl font-medium">
           <h2>Total:</h2>
-          <h2>$15.900</h2>
+          <h2>$40.800</h2>
         </div>
         <Link
           href="/"
--- components/subcomponents/CartProduct.tsx
+++ components/subcomponents/CartProduct.tsx
@@ -1,9 +1,30 @@
-const CartProdcut = () => {
+import Image from "next/image";
+import hoodieImg from "@/images/HOODIEPrueba.webp";
+import { BsFillTrash3Fill } from "react-icons/bs";
+
+const CartProduct = () => {
   return (
-    <div className="w-full h-full bg-black">
-      <div>{/* Cart Component */}</div>
+    <div className="flex justify-evenly items-center gap-4 w-full h-[150px] p-2">
+      <Image
+        src={hoodieImg}
+        alt="Hoodie"
+        className=" object-contain max-w-[100px] max-h-full"
+      />
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center items-center gap-8">
+          <h2 className=" w-2/3">HOODIE (AND CONFUSED) I (XL)</h2>
+          <BsFillTrash3Fill className="cursor-pointer text-2xl" />
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-[12px]">
+            <h3>CANTIDAD: 1</h3>
+            {/* Buttons to increase-decrease */}
+          </div>
+          <h3>$40.000</h3>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default CartProdcut;
+export default CartProduct;
