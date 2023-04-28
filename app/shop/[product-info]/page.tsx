4886d60c41d0f052cd6e5233c81d25b3ec53d199
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-27 21:50:35 -0300

    Product-info-to-buy

--- app/shop/[product-info]/page.tsx
+++ app/shop/[product-info]/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import hoodie from "@/images/HOODIEPrueba1.webp";
 import Talles from "@/components/subcomponents/Talles";
+import Footer from "@/components/Footer";
 
 const ProductInfo = () => {
   return (
-    <div className="">
-      <div className="flex flex-col gap-2">
+    <div className="p-4">
+      <div className="flex flex-col gap-4">
         <Image src={hoodie} alt="Hoodie" className="w-full" />
-        <div className="p-4 flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
           <h2 className="text-md">HOODIE (UNDEFINED AND CONFUSED) I</h2>
           <p className="text-xs font-bold">$40000</p>
         </div>
@@ -16,6 +17,10 @@ const ProductInfo = () => {
         <p>Talle</p>
         <Talles />
       </div>
+      <button className="bg-black text-white w-full py-4 mt-8 text-xs">
+        AGREGAR AL CARRITO
+      </button>
+      <Footer />
     </div>
   );
 };
